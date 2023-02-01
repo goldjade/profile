ab39d0306e88b98256815fe307fbcf7edd9f97dc
choi <bcaca0831@gmail.com>
2023-02-01 10:19:49 +0900

    useEffect 및 useRef 를 활용한 모션 작업

--- src/components/common/LayOut.js
+++ src/components/common/LayOut.js
@@ -1,20 +1,33 @@
 /** @format */
 
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
-// Layout.js : 공통 요소를 적용하기 위한 처리 컴포넌트
 const Layout = (props) => {
+  //useRef(초기값) 변수 즉 state가 아님 화면 랜더링 안됨 변수를 관리/ 값을 보관 중첩해서 사용 랜더링엔 지장 없고 html 태그 저장 윈도우 리사이즈 스크롤 체크 등
+  //그냥 html 태그 저장 하는 거라고 생각하자 편하게
+  // useSate( 초기값) state는 무엇인가 랜더링용 변수 상태가 변함
+  //아래 구문은 real Dom을 참조함 하지만 오류가 남, 아직 리얼 돔(html tag) 이 안만들어져 참조가 안됨
+  // useRef(document.querySelector("section")); //유즈이팩트 실행하고 나서 실행됨 노의미
+  // 1번 . 통상적으로 변수 만들어 쓴다고 함
+  const frame = useRef(null);
+
+  useEffect(() => {
+    //frame useRef를 활용해서 section 태그 참조 해서 css 작업
+    // frame.current  돔 요소를 가르킴
+    frame.current.classList.remove("on") //지우고
+    frame.current.classList.add("on") // 애니메이션 줄거임
+    return () => {
+      //아래 구문은 에러가 발생
+      //unmount가 되면 참조 요소가 null 이된다.
+      // frame.current.classList.remove("")
+    };
+  }, []);
   return (
-    //`백틱`을 이용해서 props로 전달된 값을 이용해서 css className 으로 활용
-    <section className={`content ${props.title}`}>
+    // 2번 상위에 만들어둔 useRef 변수 Frame을 ref 속성으로 저장
+    <section className={`content ${props.title}`} ref={frame}>
       <figure></figure>
       <div className="inner">
         <h1>{props.title}</h1>
-        {/* 
-            실제 컴포넌트내용 
-            props.children 을 이용해서
-            원하는 영역에 컴포넌트를 배치가능
-        */}
         {props.children}
       </div>
     </section>
--- src/components/sub/Community.js
+++ src/components/sub/Community.js
@@ -1,10 +1,30 @@
 /** @format */
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import LayOut from '../common/LayOut';
 
 const Community = () => {
-  return <LayOut title={"Community"}>Community</LayOut>;
+  //javascript 코드 자리
+  //컴포넌트가 마운트, 업데이트(state 변경), 언마운트 시 하고싶은 일을 작성하는 hook  useEffect,
+  //기본 형태
+  // useEffect(()=>{
+  //   //하고싶은일
+  //   //클린업 함수 : 언마운트(컴포넌트 제거)시 실행
+  //   return ()=>{
+
+  //   }
+  // },[의존성배열 : defendency Array])
+  useEffect(() => {
+    console.log('mount :컴포넌트생성');
+    //axios, html 요소선택하는 작업
+    return () => {
+      console.log('unmount:컴포넌트제거');
+    };
+  }, []);
+  return (
+    //jsx 코드 자리 : 주의사항 return(괄호)있어야함 {중괄호} 이거 아님
+    <LayOut title={'Community'}>Community</LayOut>
+  );
 };
 
 export default Community;
