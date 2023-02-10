b2ddac7e7487099763fa70955b59cf38f39ebf42
choi <bcaca0831@gmail.com>
2023-02-10 13:02:53 +0900

    useForm yup 적용

--- src/components/sub/Community.js
+++ src/components/sub/Community.js
@@ -3,9 +3,37 @@
 import React, { useEffect, useRef, useState } from 'react';
 import LayOut from '../common/LayOut';
 import { CommunityCard } from './CommunityCard';
+// 01 useForm import
+// https://react-hook-form.com/                   npm install react-hook-form
+// https://github.com/jquense/yup/tree/pre-v1     npm install -S yup
+// npm install @hookform/resolvers    hook-form이랑 yup을 연결해주는 라이브러리
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+
+//02 form 요소의 항목별 에러체크 정의
+const schema = yup.object({
+  title: yup.string().trim().required('제목을 입력해주세요'),
+  content: yup.string().trim().required('내용을 입력해주세요'),
+});
 
 const Community = () => {
-  // 데모용 데이터 생성
+  //03. useForm 생성
+  //register 각각의 form의 name의 내용을 생성한다.
+  // handleSubmit : form에서  onSubmit 할때 실행됨
+  //reset : form에서 reset 할때 실행
+  // formState:{errors}  yup 에러 출력 활용
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema), //yup과 연결시켜줌
+    mode: 'onChange', // mode가 onchange면 실행하라
+  });
+
+  // 데모용 데이터 생
   const initPost = [
     { title: 'Hello 1', content: 'Welocome To React!' },
     { title: 'Hello 2', content: 'Welocome To React!' },
@@ -13,45 +41,20 @@ const Community = () => {
     { title: 'Hello 4', content: 'Welocome To React!' },
     { title: 'Hello 5', content: 'Welocome To React!' },
   ];
-  // 출력 목록 관리 state
   const [posts, setPosts] = useState(initPost);
 
-  // 각 항목을 useState 로 사용하면
-  // 리랜더링이 많이 일어나므로 ref 로 활용한다.
-  // ref로 value 를 참조하면 리랜더링을 줄인다.
-  const input = useRef(null);
-  const contents = useRef(null);
   const inputEdit = useRef(null);
   const textareaEdit = useRef(null);
-
-  // 업데이트는 한개만 가능하도록
   const [Allowed, setAllowed] = useState(true);
 
-  const createPost = () => {
-    // 앞자리 및 뒷자리 공백을 제거하기 위해 trim() 사용
-    if (
-      input.current.value.trim() === '' ||
-      contents.current.value.trim() === ''
-    ) {
-      resetPost();
-      return alert('제목과 본문을 입력하세요.');
-    }
-    // 새로운 포스트 등록
-    // state 업데이트라서 화면 갱신
-    setPosts([
-      ...posts,
-      { title: input.current.value, content: contents.current.value },
-    ]);
-    // 입력 저장후 초기화
-    resetPost();
-
-    // 연속으로 setState 를 진행하면 1번만 된다.
-    // 연속으로 setState 를 하려면
-    // prev 콜백함수를 활용 업데이트 가능
-    // setAllowed((prev) => {return true});
+  const createPost = (data) => {
+    //data === {title:title , content:content}
+    setPosts([...posts, data]);
+    // ...register("title")
+    // ...register("content")
+    reset();
     setAllowed((prev) => true);
 
-    // 모든 목록 닫아주기
     setPosts((prev) => {
       const arr = [...prev];
       const updateArr = arr.map((item, index) => {
@@ -61,49 +64,29 @@ const Community = () => {
       return updateArr;
     });
   };
-  const resetPost = () => {
-    input.current.value = '';
-    contents.current.value = '';
-  };
+
   // 삭제기능
   const deletePost = (idx) => {
     if (!window.confirm('정말 삭제하시겠습니까?')) {
       return;
     }
-    // idx 와 동일한 순서값을 찾아서 state 업데이트
-    // 배열.filter 는 조건에 맞는
-    // 즉, true 배열에 담고, false 면 제외하고
-    // 새로운 배열을 돌려줍니다.
     setPosts(posts.filter((item, index) => idx !== index));
   };
   // 업데이트 기능
   const enableUpdate = (idx) => {
-    // 1개만 업데이트 가능하도록 처리
-    // if (Allowed === false) return;
-    // if (Allowed === false) {
-    //   return;
-    // }
     if (!Allowed) return;
-    // 토글 시켜준다.
     setAllowed(false);
 
-    // 배열들 중에 idx 에 해당하는
-    // 1개만 편집이 가능하다고 설정
-    // posts State 중에 1개를 설정한다.
     setPosts(
       posts.map((item, index) => {
         if (idx === index) {
-          // 편집이 가능해요 라고 설정하는 키생성
           item.enableUpdate = true;
-          // { title: "Hello 3", content: "Welocome To React!", enableUpdate:true  }, 2
         }
         return item;
       })
     );
   };
-  // 업데이트 취소
   const disapleUpdate = (idx) => {
-    // 업데이트 가능하도록
     setAllowed(true);
 
     setPosts(
@@ -146,23 +129,25 @@ const Community = () => {
     <LayOut title={'Community'}>
       {/* 입력폼 */}
       <div className="inputBox">
-        <form>
-          <input type="text" placeholder="제목을 입력하세요" ref={input} />
+        <form onSubmit={handleSubmit(createPost)}>
+          <input
+            type="text"
+            placeholder="제목을 입력하세요"
+            {...register('title')}
+          />
+          <span className="err">{errors.title?.message}</span>
           <br />
           <textarea
             cols="30"
             rows="5"
             placeholder="본문을 입력하세요."
-            ref={contents}
+            {...register('content')}
           ></textarea>
+          <span className="err">{errors.content?.message}</span>
           <div className="btnSet">
             {/* form 안쪽에 버튼은 type 을 정의한다. */}
-            <button type="button" onClick={resetPost}>
-              CANCEL
-            </button>
-            <button type="button" onClick={createPost}>
-              WRITE
-            </button>
+            <button type="reset">CANCEL</button>
+            <button type="submit">WRITE</button>
           </div>
         </form>
       </div>
